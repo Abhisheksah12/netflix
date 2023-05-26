97a84a73f2f729c689a483501c221e12195be9aa
Abhisheksah12 <aarav12sah@gmail.com>
2023-05-26 21:00:14 +0530

    netflix-clone

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB0RpFuEaJYX_uBT4E9GaeDSxD7MXQNLRk",
+    authDomain: "netflix-abhisek.firebaseapp.com",
+    projectId: "netflix-abhisek",
+    storageBucket: "netflix-abhisek.appspot.com",
+    messagingSenderId: "836155666877",
+    appId: "1:836155666877:web:a8f4b58644b24a3b5122d4",
+    measurementId: "G-NFDXJHN7G1"
+  };
